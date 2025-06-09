c7b7de050a6b59031720efbc26bb90810d836598
zxccursed <asobolev925@gmail.com>
2025-06-09 17:47:43 +0400

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style/app.scss'
+import './style/core.scss'
 import { Router } from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
